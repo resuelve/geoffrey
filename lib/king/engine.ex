b8d8a72f26d4e744473282fd2d41e21acf50a6d1
Poncho <poncho.811@gmail.com>
2021-05-06 16:42:58 +0200

    Adds validation and errors to the rule struct

--- lib/king/engine.ex
+++ lib/king/engine.ex
@@ -35,7 +35,7 @@ defmodule King.Engine do
   defp eval_rules(%__MODULE__{type: :cascade, rules: rules}, input) do
     rules_evaluations = Enum.map(rules, &Rule.eval(&1, input))
 
-    case Enum.all?(rules_evaluations, &elem(&1, 0)) do
+    case Enum.all?(rules_evaluations, &(&1.valid?)) do
       true ->
         rules_evaluations
 
@@ -47,8 +47,8 @@ defmodule King.Engine do
   defp eval_rules(%__MODULE__{type: :first_one, rules: rules}, input) do
     valid_rule =
       Enum.find(rules, fn rule ->
-        {eval, _} = Rule.eval(rule, input)
-        eval
+         %Rule{valid?: valid?} = Rule.eval(rule, input)
+         valid?
       end)
 
     case valid_rule do
--- lib/king/rule.ex
+++ lib/king/rule.ex
@@ -6,7 +6,10 @@ defmodule King.Rule do
     :desc,
     {:priority, 0},
     {:conditions, []},
-    {:actions, []}
+    {:actions, []},
+    :result,
+    {:valid?, true},
+    {:errors, []}
   ]
 
   @type t :: %__MODULE__{
@@ -16,6 +19,8 @@ defmodule King.Rule do
           conditions: [],
           actions: []
         }
+
+  @arity_error "A Rule action must be a function of arity 1"
 
   def new(name, desc, priority \\ 0) do
     %__MODULE__{
@@ -46,18 +51,33 @@ defmodule King.Rule do
     add_condition(rule, condition)
   end
 
-  def add_action(rule, action) do
-    new_actions = [action | rule.actions]
+  def add_action(rule, action) when is_function(action) do
+    action
+    |> :erlang.fun_info()
+    |> Keyword.get(:arity)
+    |> case do
+      1 = _arity ->
+        new_actions = [action | rule.actions]
 
-    %{rule | actions: new_actions}
+        %{rule | actions: new_actions}
+
+      _ ->
+        invalid_action(rule)
+    end
+  end
+
+  def add_action(rule, _action) do
+    invalid_action(rule)
   end
 
   def eval(rule, input) do
+    rule = update_result(rule, input)
+
     with true <- eval_conditions(rule, input) do
-      {true, run_actions(rule)}
+      run_actions(rule)
     else
       _ ->
-        {false, nil}
+        invalidate(rule)
     end
   end
 
@@ -65,13 +85,30 @@ defmodule King.Rule do
     Enum.all?(conditions, &Condition.eval(&1, input))
   end
 
-  defp run_actions(%__MODULE__{actions: actions}) do
-    Enum.map(actions, fn
-      value when is_function(value) ->
-        value.()
-
-      value ->
-        value
+  defp run_actions(%__MODULE__{actions: actions} = rule) do
+    Enum.reduce(actions, rule, fn action, updated_rule ->
+      updated_result = action.(updated_rule.result)
+      %{updated_rule | result: updated_result}
     end)
+  end
+
+  defp update_result(%__MODULE__{} = rule, data) do
+    %{rule | result: data}
+  end
+
+  defp invalid_action(rule) do
+    rule
+    |> add_error("Invalid action is not a function of arity 1")
+    |> invalidate()
+  end
+
+  defp add_error(rule, error) do
+    Map.update(rule, :errors, [], fn errors ->
+      [error | errors]
+    end)
+  end
+
+  defp invalidate(rule) do
+    %{rule | valid?: false}
   end
 end
