ad0f4a2fbcdf163dfe79ee8036b81a76be67c0ba
poncho <poncho.811@gmail.com>
2023-05-30 21:22:42 +0200

    Removes extra Enum.map I actually don't know why this was here, it was breaking all tests

--- lib/geoffrey/parsers/text.ex
+++ lib/geoffrey/parsers/text.ex
@@ -48,7 +48,6 @@ defmodule Geoffrey.Parsers.Text do
     |> String.split("\n")
     |> Enum.map(&parse_condition/1)
     |> Enum.filter(&elem(&1, 0))
-    |> Enum.map(&elem(&1, 1))
   end
 
   # Crea una condicion a partir de una cadena de caracteres
