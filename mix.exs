61589dd6565896ad2af4b1aa7839cb04fc40e150
Erick Navarro <erick@navarro.io>
2023-06-08 11:32:18 -0600

    Bump version 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geoffrey.MixProject do
   def project do
     [
       app: :geoffrey,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
